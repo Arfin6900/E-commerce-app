6d6fe000de112d2cff073c87380aa40b72faa6fb
Ayan Murad <41545001+ayanmurad987@users.noreply.github.com>
2024-01-26 12:32:35 +0500

    added-little-fix

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,6 +6,8 @@ const Footer = () => {
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">JoqCafe™</a>. All Rights Reserved.
     </span>
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">Develop with ❤️ by <a href="https://ayanmurad.com/" class="hover:underline">Ayan Murad™</a>
+    </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
             <a href="#" class="hover:underline me-4 md:me-6">About</a>
